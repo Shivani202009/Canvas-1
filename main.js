56c732ba68a3c49c98a2bf03653ae09bcb3dd9a3
Shivani202009 <73746302+Shivani202009@users.noreply.github.com>
2021-07-01 12:21:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+function preload() {
+    
+}
+
+function setup() {
+    canvas=createCanvas(400,400) 
+    canvas.position(100,200) 
+    
+  }
+
+  function draw() {
+    fill("green")
+    stroke("black")
+    rect(350,0,50,70)
+    fill("red")
+    stroke("black")
+    rect(340,70,100,70)
+    fill("blue")
+    stroke("black")
+    rect(360,140,100,70)
+    fill("green")
+    stroke("black")
+    rect(350,210,50,70)
+    fill("red")
+    stroke("black")
+    rect(360,280,100,150)
+    fill("red")
+    stroke("black")
+    rect(-50,1,100,150)
+    fill("green")
+    stroke("black")
+    rect(-30,100,100,70)
+    fill("blue")
+    stroke("black")
+    rect(-50,170,100,70)
+    fill("red")
+    stroke("black")
+    rect(1,210,50,160)
+    fill("green")
+    stroke("black")
+    rect(1,330,50,70)
+    fill("yellow")
+    stroke("black")
+    rect(52,350,100,70)
+    fill("blue")
+    stroke("black")
+    rect(153,370,100,70)
+    fill("green")
+    stroke("black")
+    rect(250,350,110,90)
+  }
